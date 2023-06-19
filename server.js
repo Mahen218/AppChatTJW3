7f20773564539b839b609ca6541d821346c64e72
tobiokagae <jovapertama@gmail.com>
2023-06-19 21:56:48 +0800

    acli nich

--- server.js
+++ server.js
@@ -1,3 +1,40 @@
+// const express = require('express');
+// const http = require('http');
+// const socketIO = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIO(server);
+
+// // Menyajikan file statis dari folder 'public'
+// app.use(express.static('public'));
+
+// // Menangani koneksi socket
+// io.on('connection', (socket) => {
+//   const userAgent = socket.request.headers['user-agent'];
+//   console.log('User terhubung dari perangkat:', userAgent);
+
+//   // Menangani pesan yang diterima
+//   socket.on('chat message', (message) => {
+//     console.log('Pesan diterima:', message);
+
+//     // Mengirim pesan ke semua pengguna terhubung
+//     io.emit('chat message', message);
+//   });
+
+//   // Menangani pemutusan koneksi socket
+//   socket.on('disconnect', () => {
+//     console.log('User terputus');
+//   });
+// });
+
+// // Menjalankan server
+// const port = 3000;
+// const host = '192.168.1.4'; // Menggunakan host universal
+// server.listen(port, host, () => {
+//   console.log(`Server berjalan di http://${host}:${port}`);
+// });
+
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -15,11 +52,11 @@ io.on('connection', (socket) => {
   console.log('User terhubung dari perangkat:', userAgent);
 
   // Menangani pesan yang diterima
-  socket.on('chat message', (message) => {
-    console.log('Pesan diterima:', message);
+  socket.on('chat message', (data) => {
+    console.log('Pesan diterima:', data);
 
-    // Mengirim pesan ke semua pengguna terhubung
-    io.emit('chat message', message);
+    // Mengirim pesan beserta nama client ke semua pengguna terhubung
+    io.emit('chat message', data);
   });
 
   // Menangani pemutusan koneksi socket
@@ -30,7 +67,7 @@ io.on('connection', (socket) => {
 
 // Menjalankan server
 const port = 3000;
-const host = '192.168.1.4'; // Menggunakan host universal
+const host = '192.168.202.112'; // Menggunakan host universal
 server.listen(port, host, () => {
   console.log(`Server berjalan di http://${host}:${port}`);
 });
