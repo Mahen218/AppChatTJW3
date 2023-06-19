ccba1d59f45a91b291a190a6cdb9af828f910991
tobiokagae <128783688+tobiokagae@users.noreply.github.com>
2023-06-19 22:04:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIO = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server);
-
-// // Menyajikan file statis dari folder 'public'
-// app.use(express.static('public'));
-
-// // Menangani koneksi socket
-// io.on('connection', (socket) => {
-//   const userAgent = socket.request.headers['user-agent'];
-//   console.log('User terhubung dari perangkat:', userAgent);
-
-//   // Menangani pesan yang diterima
-//   socket.on('chat message', (message) => {
-//     console.log('Pesan diterima:', message);
-
-//     // Mengirim pesan ke semua pengguna terhubung
-//     io.emit('chat message', message);
-//   });
-
-//   // Menangani pemutusan koneksi socket
-//   socket.on('disconnect', () => {
-//     console.log('User terputus');
-//   });
-// });
-
-// // Menjalankan server
-// const port = 3000;
-// const host = '192.168.1.4'; // Menggunakan host universal
-// server.listen(port, host, () => {
-//   console.log(`Server berjalan di http://${host}:${port}`);
-// });
-
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -59,15 +22,15 @@ io.on('connection', (socket) => {
     io.emit('chat message', data);
   });
 
-  // Menangani pemutusan koneksi socket
+  // Disconnect
   socket.on('disconnect', () => {
     console.log('User terputus');
   });
 });
 
-// Menjalankan server
+// Running server
 const port = 3000;
-const host = '192.168.202.112'; // Menggunakan host universal
+const host = '192.168.202.112';
 server.listen(port, host, () => {
   console.log(`Server berjalan di http://${host}:${port}`);
 });
